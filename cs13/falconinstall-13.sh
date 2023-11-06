7ed73fc83317780911ab6f51babc6f9d80cf01ba
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-06 13:58:12 -0600

    Update falconinstall-13.sh

--- cs13/falconinstall-13.sh
+++ cs13/falconinstall-13.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo port install wget 
-sudo wget https://github.com/scriptsbu/mac-os/raw/main/cs13/GovLaggar_Release_standard-p_16403.pkg
+sudo curl -O https://github.com/scriptsbu/mac-os/raw/main/cs13/GovLaggar_Release_standard-p_16403.pkg
 sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_16403.pkg -target /
 sudo /Applications/Falcon.app/Contents/Resources/falconctl license 6C145CE47FC14784904D6E1B2F9B5A4C-5F
 sudo /Applications/Falcon.app/Contents/Resources/falconctl load
