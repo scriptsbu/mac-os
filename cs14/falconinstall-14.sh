6ccc608850791b7d3e8cf5a2b95cf845653b4bc0
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-06 14:09:01 -0600

    Update falconinstall-14.sh

--- cs14/falconinstall-14.sh
+++ cs14/falconinstall-14.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo wget https://github.com/scriptsbu/mac-os/raw/main/cs14/GovLaggar_Release_standard-p_17506.pkg
+cd Downloads
+sudo curl -O https://github.com/scriptsbu/mac-os/raw/main/cs14/GovLaggar_Release_standard-p_17506.pkg
 sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_17506.pkg -target /
 sudo /Applications/Falcon.app/Contents/Resources/falconctl license 6C145CE47FC14784904D6E1B2F9B5A4C-5F
 sudo /Applications/Falcon.app/Contents/Resources/falconctl load
