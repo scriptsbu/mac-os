eba7041813821a685b1dc452ebb339eca1e53c31
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-06 13:27:39 -0600

    Add files via upload

---
+++ cs13/falconinstall.sh
@@ -0,0 +1,5 @@
+sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_16403.pkg -target /
+
+sudo /Applications/Falcon.app/Contents/Resources/falconctl license 6C145CE47FC14784904D6E1B2F9B5A4C-5F
+
+sudo /Applications/Falcon.app/Contents/Resources/falconctl load
