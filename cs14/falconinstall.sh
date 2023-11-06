d13a6691fd73770410eb2a3297475dfcd1861636
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-06 13:35:31 -0600

    Update and rename falconinstall.sh to falconinstall-14.sh

--- cs14/falconinstall.sh
+++
@@ -1,5 +0,0 @@
-sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_17506 -target /
-
-sudo /Applications/Falcon.app/Contents/Resources/falconctl license 6C145CE47FC14784904D6E1B2F9B5A4C-5F
-
-sudo /Applications/Falcon.app/Contents/Resources/falconctl load
