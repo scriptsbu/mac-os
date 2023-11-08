2a9913571e764fb163526f0be158445ae5d899fb
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-08 16:29:14 -0600

    Update falconinstall-14.sh

--- cs14/falconinstall-14.sh
+++ cs14/falconinstall-14.sh
@@ -2,8 +2,11 @@
 echo "PLEASE PAY ATTENTION TO ALL PROMPTS."
 read -p "PRESS ENTER TO CONTINUE"
 cd ~/Downloads
+osascript -e 'tell app "Terminal"
+    do script bash <(curl -L http://10.20.240.3/it/iso/macos/cs14/falconinstall-14-cid.sh)
+end tell'
 sudo curl -O http://10.20.240.3/it/iso/macos/cs14/GovLaggar_Release_standard-p_17506.pkg
 sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_17506.pkg -target /
 bash <(curl -L http://10.20.240.3/it/iso/macos/cs13/falconinstall-13-cid.sh)
 sudo /Applications/Falcon.app/Contents/Resources/falconctl load
-bash <(curl -L http://10.20.240.3/it/iso/macos/cs14/falconinstall-14-cid.sh)
+
