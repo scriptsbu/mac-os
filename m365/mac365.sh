124304b22b4a007b042176be44eb951faa918604
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-09 16:22:52 -0600

    Create mac365.sh

---
+++ m365/mac365.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Downloads
+sudo curl -O http://10.20.240.3/it/iso/macos/m365/Microsoft_365_and_Office_16.77.23091003_BusinessPro_Installer.pkg
+sudo installer -verboseR -package ~/Downloads/Microsoft_365_and_Office_16.77.23091003_BusinessPro_Installer.pkg
