97240ace294a55eee4c5df16387d3c2f6d104193
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-09 16:55:52 -0600

    Update macmenu.sh

--- macmenu.sh
+++ macmenu.sh
@@ -6,9 +6,29 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Option 1-CROWDSTRIKE")
-#        bash <(curl -Ls http://10.20.240.3/it/script/u20logstart.sh)
+echo "CHOOSE FROM THE FOLLOWING OPTIONS TO INSTALL:"
+PS3=''
+options=("Option 1-CS13" "Option 2-CS14" "Option 3-Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1-CS13")
+        bash <(curl -Ls https://github.com/scriptsbu/mac-os/raw/main/cs13/falconinstall-13.sh)
                 echo "MENU IN PROGRESS, COMEBACK LATER"
                 exit
+            ;;
+        "Option 2-CS14")
+        bash <(curl -Ls https://github.com/scriptsbu/mac-os/raw/main/cs14/falconinstall-14.sh)
+                echo "INSTALLATION IS DONE!"
+                read -p "PRESS ENTER TO RETURN TO THE MAIN MENU"              
+            ;;
+        "Quit")
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+exit
             ;;
         "Option 2-MS365")
         bash <(curl -Ls https://github.com/scriptsbu/mac-os/raw/main/m365/mac365.sh)
