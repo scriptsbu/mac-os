4eff86c74a3784378617c7e21f8bdd2cb00a653b
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-12-11 16:32:51 -0600

    Update macpapercut14.sh

--- papercut/macpapercut14.sh
+++ papercut/macpapercut14.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Downloads
-sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client-14%5bpapercut.torc.tech%5d.dmg
+sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client-14[papercut.torc.tech].dmg
 sudo installer -verboseR -package ~/Downloads/macpapercut14.dmg-target /
