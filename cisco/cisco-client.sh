b0f2b428c940264acebf617eef628f5a3f68e444
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-12-15 12:15:25 -0600

    Create cisco-client.sh

---
+++ cisco/cisco-client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Downloads
+sudo curl -O http://10.20.240.3/it/iso/macos/cisco-client/anyconnect-macos-4.10.01075-core-vpn-webdeploy-k9.dmg
+sudo installer -verboseR -package ~/Downloads/anyconnect-macos-4.10.01075-core-vpn-webdeploy-k9.dmg -target /
