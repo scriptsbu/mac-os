3e6f52ea4ac5e610c889d879be3c421bd811c810
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 09:53:27 -0600

    Update falconinstall-13.sh

--- cs13/falconinstall-13.sh
+++ cs13/falconinstall-13.sh
@@ -3,4 +3,5 @@ cd ~/Downloads
 sudo curl -O http://10.20.240.3/it/iso/macos/cs13/GovLaggar_Release_standard-p_16403.pkg
 sudo installer -verboseR -package ~/Downloads/GovLaggar_Release_standard-p_16403.pkg -target /
 bash <(curl -L http://10.20.240.3/it/iso/macos/cs13/falconinstall-13-cid.sh)
-sudo /Applications/Falcon.app/Contents/Resources/falconctl load
+cd /Applications/Falcon.app/Contents/MacOS/
+Open Falcon
