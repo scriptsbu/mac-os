05ed701ec963bc9ccf10160c3a7ee998e51113d4
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 10:10:37 -0600

    Update cisco-client.sh

--- cisco/cisco-client.sh
+++ cisco/cisco-client.sh
@@ -2,5 +2,5 @@
 cd ~/Downloads
 sudo curl -O http://10.20.240.3/it/iso/macos/cisco-client/anyconnect-macos-4.10.01075-core-vpn-webdeploy-k9.dmg
 sudo installer -verboseR -package ~/Downloads/anyconnect-macos-4.10.01075-core-vpn-webdeploy-k9.dmg -target /
-cd /Applications/
+cd /Applications/Cisco
 open Cisco\ AnyConnect\ Secure\ Mobility\ Client.app
