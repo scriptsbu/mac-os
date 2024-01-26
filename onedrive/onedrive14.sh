6c7840b6d8cc96864ade047ce88be738df2b7cf0
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 10:06:27 -0600

    Update onedrive14.sh

--- onedrive/onedrive14.sh
+++ onedrive/onedrive14.sh
@@ -2,3 +2,4 @@
 cd ~/Downloads
 sudo curl -O http://10.20.240.3/it/iso/macos/OneDrive/OneDrive.pkg
 sudo installer -verboseR -package ~/Downloads/OneDrive.pkg -target /
+open /Applications/OneDrive.app
