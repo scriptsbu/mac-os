803ec7c0ae763c7b57ec690f34296796746b1be7
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 10:04:46 -0600

    Update ms365-14.sh

--- ms365/ms365-14.sh
+++ ms365/ms365-14.sh
@@ -2,3 +2,4 @@
 cd ~/Downloads
 sudo curl -O http://10.20.240.3/it/iso/macos/m365-14/Microsoft_365_and_Office_16.78.23102801_BusinessPro_Installer.pkg
 sudo installer -verboseR -package ~/Downloads/Microsoft_365_and_Office_16.78.23102801_BusinessPro_Installer.pkg -target /
+open /Applications/Microsoft\ Outlook.app
