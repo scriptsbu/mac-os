ad3da6d358602a65863b5182bc761f4a2d4f108a
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 10:32:16 -0600

    Update slack.sh

--- slack/slack.sh
+++ slack/slack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/Downloads
 sudo curl -O http://10.20.240.3/it/iso/macos/Slack/Slack-4.35.126-macOS.dmg
-#sudo installer -verboseR -package ~/Downloads/Slack-4.35.126-macOS.dmg -target /
 open Slack-4.35.126-macOS.dmg
+#sudo installer -verboseR -package ~/Downloads/Slack-4.35.126-macOS.dmg -target /
