6254e3b91361dcfab407b2748eee5fefabbe687b
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-01 10:34:24 -0500

    Update macpapercut.sh

--- papercut/macpapercut.sh
+++ papercut/macpapercut.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd ~/Downloads
-#sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
-#sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/paper15/pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
-sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/paper15/PaperCut%20Print%20Deploy%20Client.pkg
-sudo installer -verboseR -package ~/Downloads/PaperCut%20Print%20Deploy%20Client.pkg -target /
-#open pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
+sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
+open pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
