33dddd79215c8b7e69fa24ac769ec002cfaa5e28
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-02 19:31:45 -0500

    Update macpapercut.sh

--- papercut/macpapercut.sh
+++ papercut/macpapercut.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
-cd ~/Downloads
-sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
-open pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg
+
+# Set the download URL
+download_url="http://10.20.240.3/it/iso/macos/papercut15/pc-print-deploy-client%5bpapercut.torc.tech%5d.dmg"
+
+# Set the destination file name and path
+file_name="pc-print-deploy-client.dmg"
+file_path="/Users/$USER/Downloads/$file_name"
+
+# Download the file
+echo "Downloading $file_name..."
+curl -o "$file_path" "$download_url"
+
+if [ $? -eq 0 ]; then
+    echo "Download complete."
+    
+    # Mount the DMG file
+    echo "Mounting the DMG file..."
+    hdiutil attach "$file_path"
+    
+    # Find the package file inside the mounted volume
+    package_path=$(find /Volumes/ -name "*.pkg" -type f -print -quit)
+    
+    if [ -n "$package_path" ]; then
+        # Install the package
+        echo "Installing the package..."
+        sudo installer -pkg "$package_path" -target /
+        
+        if [ $? -eq 0 ]; then
+            echo "Installation successful."
+        else
+            echo "Installation failed. Error code: $?"
+        fi
+    else
+        echo "Failed to find the package file inside the DMG."
+    fi
+    
+    # Unmount the DMG file
+    echo "Unmounting the DMG file..."
+    hdiutil detach "/Volumes/$(basename "${package_path%.*}")"
+else
+    echo "Download failed. Error code: $?"
+fi
