594c89e7507d19dd3bda2cb8f92df69a87187d46
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-02 19:48:10 -0500

    Delete papercut/macpapercut14.sh

--- papercut/macpapercut14.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Downloads
-sudo curl -O http://10.20.240.3/it/iso/macos/Papercut/pc-print-deploy-client-14%5bpapercut.torc.tech%5d.dmg
-open pc-print-deploy-client-14%5bpapercut.torc.tech%5d.dmg
