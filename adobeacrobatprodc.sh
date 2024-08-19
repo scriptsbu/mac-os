e643d9faa419b9316a1c612857fee48cf1d8d742
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-08-19 18:37:07 -0500

    Create adobeacrobatprodc.sh
    The following script will download and install Adobe Acrobat Pro DC.

---
+++ adobeacrobatprodc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the download URL
+url="https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg"
+
+# Set the destination file name
+filename="Acrobat_DC_Web_WWMUI.dmg"
+
+# Download the DMG file
+echo "Downloading Acrobat DC..."
+curl -o "$filename" "$url"
+
+# Mount the DMG file
+echo "Mounting the DMG file..."
+hdiutil attach "$filename"
+
+# Find the package path
+package_path=$(find "/Volumes" -name "*.pkg" -print -quit)
+
+# Install the app
+echo "Installing Acrobat DC..."
+sudo installer -pkg "$package_path" -target /
+
+# Unmount the DMG file
+echo "Unmounting the DMG file..."
+diskutil unmount force "/Volumes/Acrobat DC"
+
+rm "$filename"
+
+echo "Acrobat DC installation complete."
