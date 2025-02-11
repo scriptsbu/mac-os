cdad902dfdd918278b53f88d5688269086d27d45
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2025-02-10 20:22:23 -0600

    Update logioptions.sh

--- logioptions.sh
+++ logioptions.sh
@@ -96,7 +96,7 @@ install_logioptions() {
     unzip -o "$installer_zip" -d /tmp
 
     # Install logioptionsplus
-    installer_app="/tmp/logioptionsplus_installer/logioptionsplus_installer.app"
+    installer_app="/tmp/logioptionsplus_installer.app"
     if [ -d "$installer_app" ]; then
         echo "Installing logioptionsplus..."
         open "$installer_app"
